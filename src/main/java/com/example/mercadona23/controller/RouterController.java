6e25d6ccd0208c6ca9fcdc60ded858650a04f5f9
Alexis Ferrand <alxferrand@yahoo.fr>
2023-03-23 13:50:12 +0100

    Creation Projet Spring-Boot
    Modification "application.properties"
    Ajout dossier "tempFiles" & "uploadsFiles"

--- src/main/java/com/example/mercadona23/controller/RouterController.java
+++ src/main/java/com/example/mercadona23/controller/RouterController.java
@@ -1,2 +1,14 @@
-package com.example.mercadona23.controller;public class RouterController {
+package com.example.mercadona23.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RouterController {
+
+    @GetMapping("/")
+    public String getHome(){
+        return "Hello World";
+    }
 }

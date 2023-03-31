752d91f09ffafa47aa6e511839e1ea4fe52a3cbb
Alexis Ferrand <alxferrand@yahoo.fr>
2023-03-31 09:24:40 +0200

    US3 - Done
    T3.1 : do-test-valide
    T3.2 : do-test-valide
    T3.3 : do-test-valide
    T3.4 : do-test-valide
    T3.5 : do-test-valide
    T3.2 : Modification fonctionnement : liste catégories envoyée avec liste produits
    HTML et JS tested it live with navigator
    JAVA function tested by Test.class

--- src/main/java/com/example/mercadona23/controller/RouterController.java
+++ src/main/java/com/example/mercadona23/controller/RouterController.java
@@ -1,11 +1,18 @@
 package com.example.mercadona23.controller;
 
+import com.example.mercadona23.daoService.CategoriesDao;
 import com.example.mercadona23.daoService.ProductsDao;
+import com.example.mercadona23.model.Articles;
+import com.example.mercadona23.model.Products;
 import com.example.mercadona23.service.GeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -13,6 +20,8 @@ public class RouterController {
 
     @Autowired
     ProductsDao productsDao;
+    @Autowired
+    CategoriesDao categoriesDao;
     @Autowired
     GeneratorService generatorService;
 
@@ -22,9 +31,16 @@ public class RouterController {
         return "home";
     }
 
-    @GetMapping("/getCatalogue")
-    public String getCatalogue(Model model){
-        model.addAttribute("articlesList",generatorService.generateArticlesList(productsDao.getProducts()));
+    @GetMapping("/getCatalogue/{category}")
+    public String getCatalogue(Model model,@PathVariable("category") String categoryName){
+        List<Articles> articlesList = new ArrayList<>();
+        if (!categoryName.equals("all")){
+                articlesList = generatorService.generateArticlesList(productsDao.getProductsByCategoryName(categoryName));
+        }else {
+            articlesList = generatorService.generateArticlesList(productsDao.getProducts());
+        }
+        model.addAttribute("articlesList",articlesList);
+        model.addAttribute("categoriesList",categoriesDao.getCategories());
         return "catalogue";
     }
 }
